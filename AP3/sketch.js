92d0332cd1da98b6f0236eed328c05bff6d19ec2
Ricardo Velasquez <ricardovelasquez681@gmail.com>
2022-02-15 18:49:42 -0800

    q

---
+++ AP3/sketch.js
@@ -0,0 +1,13 @@
+//Ricardo Velasquez
+//
+
+function setup() {
+  cnv = createCanvas(windowWidth - 20, windowHeight - 60);
+  cnv.position((windowWidth - width) / 2, 30);
+
+}
+
+function draw() {
+  background(245);
+
+}
