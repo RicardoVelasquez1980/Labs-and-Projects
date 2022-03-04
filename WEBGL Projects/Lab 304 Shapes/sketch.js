6606928844d702ceb1d659c08e63ad3f25f13ac6
Ricardo Velasco <ricardovelasquez681@gmail.com>
2022-03-04 13:07:18 -0800

    q

---
+++ WEBGL Projects/Lab 304 Shapes/sketch.js
@@ -0,0 +1,13 @@
+//Ricardo Velasquez
+//3D Shapes
+
+function setup() {
+  var cnv = createCanvas(900, 700, WEBGL);
+  cnv.position((windowWidth - width) / 2, 30);
+
+}
+
+function draw(){
+  background(255);
+
+}
