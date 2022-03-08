f5e850640b593893946dfa811311761455d9543e
Ricardo Velasco <ricardovelasquez681@gmail.com>
2022-03-08 08:47:51 -0800

    q

---
+++ circleattempt/circle.js
@@ -0,0 +1,23 @@
+//Ricardo Velasquez
+//circleattempt
+
+class Circle extends p5.Vector{
+  constructor(){
+    super();
+  }
+
+  run(){
+    this.render();
+    this.checkCollision();
+    this.giveBirth();
+  }
+
+  render(){
+
+  }
+
+  checkCollision(){
+
+  }
+
+}
