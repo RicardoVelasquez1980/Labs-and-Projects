4576a43487a95cb993d0aa7471e204289aef6750
Ricardo Velasquez <ricardovelasquez681@gmail.com>
2022-03-21 18:57:55 -0700

    FINALLLLLLLLYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY

--- Lab 314 Platform 1/gameName.js
+++ Lab 314 Platform 1/gameName.js
@@ -6,85 +6,161 @@ class GameName {
   constructor() {
     this.player;
     this.platforms;
-    
+
     this.init();
 
   }
 
-  loadPlatforms(qty) {
+  // loadPlatforms(qty) {
+  //   let platformArr = [];
+  //
+  //   for (let i = 0; i < qty; i++) {
+  //     let lngth = random(80, 150);
+  //     let spacing = random(30, 60);
+  //     let distance;
+  //     let distanceX;
+  //     let distanceY;
+  //
+  //     //Randomized Platform Generation#####
+  //     // let x = (i * lngth) + spacing;
+  //     // let y = random(height * 0.25, height * 0.75);
+  //     // platformArr[i] = new Platform(x, y, lngth);
+  //
+  //     //Artificial Unintelligent Platform Generation#####
+  //     let x = (i * lngth) + spacing;
+  //     let y = random(height * 0.25, height * 0.75);
+  //     platformArr[i] = new Platform(x, y, lngth);
+  //     // platformArr.push(new Platform(x, y, lngth));
+  //
+  //     if (platformArr.length !== 1) {
+  //       //Originally Going Backwards Bc Of The Use Of SPLICE
+  //       //Now It Works Well Due To New Platforms Checking With Old Platforms
+  //       for (let j = platformArr.length - 1; j >= 0; j--) {
+  //         if (platformArr[j] !== platformArr[i]) {
+  //           distance = platformArr[j].dist(platformArr[i]);
+  //           distanceX = abs(platformArr[i].x - platformArr[j].x);
+  //           distanceY = abs(platformArr[i].y - platformArr[j].y);
+  //
+  //           if (distance < lngth + spacing) {
+  //             if (distanceY < platformArr[i].width * 4) {
+  //               console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
+  //
+  //               if (distanceX < (lngth + platformArr[j].length) / 2) {
+  //                 console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
+  //                 console.log("i: " + i + " OLD X POS: " + x);
+  //                 console.log("i: " + i + " OLD Y POS: " + y);
+  //                 console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
+  //                 console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
+  //
+  //                 while (distanceY < platformArr[i].width * 4 &&
+  //                   distanceX < (lngth + platformArr[j].length) / 2) {
+  //                     lngth = random(80, 150);
+  //                     spacing = random(30, 60);
+  //                     x = (i * lngth) + spacing;
+  //                     y = random(height * 0.25, height * 0.75);
+  //                     platformArr[i] = new Platform(x, y, lngth);
+  //
+  //                     distanceX = abs(platformArr[i].x - platformArr[j].x);
+  //                     distanceY = abs(platformArr[i].y - platformArr[j].y);
+  //
+  //                     console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
+  //                     console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
+  //                     console.log("i: " + i + " OLD X POS: " + x);
+  //                     console.log("i: " + i + " OLD Y POS: " + y);
+  //                     console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
+  //                     console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
+  //
+  //                   }
+  //
+  //                 }
+  //
+  //                 console.log("");
+  //
+  //               }
+  //
+  //             }
+  //
+  //           }
+  //
+  //         }
+  //
+  //       }
+  //
+  //     }
+  //
+  //     return platformArr;
+  //
+  //   }
+
+  //P Means PREVIOUS#####
+  loadPlatforms(typ, qty, platformArrP, lngthP, spacingP, xP, yP){
     let platformArr = [];
+    let lngth;
+    let spacing;
+    let x;
+    let y;
+    let distance;
+    let distanceX;
+    let distanceY;
 
-    for (let i = 0; i < qty; i++) {
-      let lngth = random(80, 150);
-      let spacing = random(30, 60);
-      let distance;
-      let distanceX;
-      let distanceY;
+    for (let i = 0; i < qty; i++){
+      if (typ === "LOAD"){
+        lngth = random(80, 150);
+        spacing = random(30, 60);
 
-      //Randomized Platform Generation#####
-      // let x = (i * lngth) + spacing;
-      // let y = random(height * 0.25, height * 0.75);
-      // platformArr[i] = new Platform(x, y, lngth);
+        x = (i * lngth) + spacing;
+        y = random(height * 0.25, height * 0.75);
+        platformArr[i] = new Platform(x, y, lngth);
 
-      //Artificial Unintelligent Platform Generation#####
-      let x = (i * lngth) + spacing;
-      let y = random(height * 0.25, height * 0.75);
-      platformArr[i] = new Platform(x, y, lngth);
-      // platformArr.push(new Platform(x, y, lngth));
+        if (platformArr.length > 1){
+          this.loadPlatforms("REGEN", platformArr.length, platformArr, lngth, spacing, x, y);
 
-      if (platformArr.length !== 1) {
-        //Originally Going Backwards Bc Of The Use Of SPLICE
-        //Now It Works Well Due To New Platforms Checking With Old Platforms
-        for (let j = platformArr.length - 1; j >= 0; j--) {
-          if (platformArr[j] !== platformArr[i]) {
+        }
+
+      } else if (typ === "REGEN"){
+        platformArr = platformArrP;
+        lngth = lngthP;
+        spacing = spacingP;
+        x = xP;
+        y = yP;
+        let changed = 0;//Decides Whether A Regen Check Needs To Be Runned Again#####
+
+        for (let j = platformArr.length - 1; j >= 0; j--){
+          if (platformArr[j] !== platformArr[i]){
             distance = platformArr[j].dist(platformArr[i]);
             distanceX = abs(platformArr[i].x - platformArr[j].x);
             distanceY = abs(platformArr[i].y - platformArr[j].y);
 
-            if (distance < lngth + spacing) {
-              // if (distanceX < lngth){
-              //   console.log(i + " TOO CLOSE X TO " + j);
-              //
-              // }
-              //
-              // if (distanceY < platformArr[i].width * 4){
-              //   console.log(i + " TOO CLOSE Y TO " + j);
-              //
-              // }
-
-              if (distanceY < platformArr[i].width * 4) {
+            if (distance < lngth + spacing){
+              if (distanceY < platformArr[i].width * 4){
                 console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
 
-                if (distanceX < (lngth + platformArr[j].length) / 2) {
+                if (distanceX < (lngth + platformArr[j].length) / 2){
                   console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
                   console.log("i: " + i + " OLD X POS: " + x);
                   console.log("i: " + i + " OLD Y POS: " + y);
                   console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
                   console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
 
-                  // lngth = random(80, 150);
-                  // spacing = random(30, 60);
-                  // x = (i * lngth) + spacing;
-                  // y = random(height * 0.25, height * 0.75);
-                  // platformArr[i] = new Platform(x, y, lngth);
+                  changed++;
 
                   while (distanceY < platformArr[i].width * 4 &&
                     distanceX < (lngth + platformArr[j].length) / 2) {
-                    lngth = random(80, 150);
-                    spacing = random(30, 60);
-                    x = (i * lngth) + spacing;
-                    y = random(height * 0.25, height * 0.75);
-                    platformArr[i] = new Platform(x, y, lngth);
+                      lngth = random(80, 150);
+                      spacing = random(30, 60);
+                      x = (i * lngth) + spacing;
+                      y = random(height * 0.25, height * 0.75);
+                      platformArr[i] = new Platform(x, y, lngth);
 
-                    distanceX = abs(platformArr[i].x - platformArr[j].x);
-                    distanceY = abs(platformArr[i].y - platformArr[j].y);
+                      distanceX = abs(platformArr[i].x - platformArr[j].x);
+                      distanceY = abs(platformArr[i].y - platformArr[j].y);
 
-                    console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
-                    console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
-                    console.log("i: " + i + " OLD X POS: " + x);
-                    console.log("i: " + i + " OLD Y POS: " + y);
-                    console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
-                    console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
+                      console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
+                      console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
+                      console.log("i: " + i + " OLD X POS: " + x);
+                      console.log("i: " + i + " OLD Y POS: " + y);
+                      console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
+                      console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
 
                   }
 
@@ -94,13 +170,13 @@ class GameName {
 
               }
 
-
-
             }
 
           }
 
         }
+
+        if (changed > 0) this.loadPlatforms("REGEN", platformArr.length, platformArr, lngth, spacing, x, y);
 
       }
 
@@ -110,38 +186,38 @@ class GameName {
 
   }
 
-  init() {
-    this.platforms = this.loadPlatforms(48);
+    init() {
+      this.platforms = this.loadPlatforms("LOAD", 48);
 
-    this.player = new Player(this.platforms[0].x + (this.platforms[0].length / 2), 0);
+      this.player = new Player(this.platforms[0].x + (this.platforms[0].length / 2), 0);
 
-  }
+    }
 
-  run() {
-    this.render();
-    this.update();
+    run() {
+      this.render();
+      this.update();
 
-  }
+    }
 
-  render() {
-    this.player.render();
+    render() {
+      this.player.render();
 
-    for (let i = 0; i < this.platforms.length; i++) {
-      this.platforms[i].render(i);
+      for (let i = 0; i < this.platforms.length; i++) {
+        this.platforms[i].render(i);
+
+      }
+
+    }
+
+    update() {
+      this.player.update();
+
+      for (let i = 0; i < this.platforms.length; i++) {
+        this.platforms[i].update();
+
+      }
 
     }
 
   }
-
-  update() {
-    this.player.update();
-
-    for (let i = 0; i < this.platforms.length; i++) {
-      this.platforms[i].update();
-
-    }
-
-  }
-
-}
-//End Class GameName##########
+  //End Class GameName##########
--- Lab 314 Platform 1/platform.js
+++ Lab 314 Platform 1/platform.js
@@ -66,3 +66,120 @@ class Platform extends p5.Vector{
 
 }
 //End Class Platform##########
+
+//P Means PREVIOUS#####
+// loadPlatforms(typ, qty, platformArrP, lngthP, spacingP, xP, yP){
+//   let platformArr = [];
+//   let lngth;
+//   let spacing;
+//   let x;
+//   let y;
+//   let distance;
+//   let distanceX;
+//   let distanceY;
+//
+//   if (typ === "LOAD"){
+//     for (let i = 0; i < qty; i++){
+//       lngth = random(80, 150);
+//       spacing = random(30, 60);
+//       x = (i * lngth) + spacing;
+//       y = random(height * 0.25, height * 0.75);
+//       platformArr[i] = new Platform(x, y, lngth);
+//
+//     }
+//
+//   } else if (typ === "REGEN"){}
+//
+//   return platformArr;
+//
+// }
+//
+
+
+
+
+
+
+
+
+
+// loadPlatforms(qty) {
+//   let platformArr = [];
+//
+//   for (let i = 0; i < qty; i++) {
+//     let lngth = random(80, 150);
+//     let spacing = random(30, 60);
+//     let distance;
+//     let distanceX;
+//     let distanceY;
+//
+//     //Randomized Platform Generation#####
+//     // let x = (i * lngth) + spacing;
+//     // let y = random(height * 0.25, height * 0.75);
+//     // platformArr[i] = new Platform(x, y, lngth);
+//
+//     //Artificial Unintelligent Platform Generation#####
+//     let x = (i * lngth) + spacing;
+//     let y = random(height * 0.25, height * 0.75);
+//     platformArr[i] = new Platform(x, y, lngth);
+//     // platformArr.push(new Platform(x, y, lngth));
+//
+//     if (platformArr.length !== 1) {
+//       //Originally Going Backwards Bc Of The Use Of SPLICE
+//       //Now It Works Well Due To New Platforms Checking With Old Platforms
+//       for (let j = platformArr.length - 1; j >= 0; j--) {
+//         if (platformArr[j] !== platformArr[i]) {
+//           distance = platformArr[j].dist(platformArr[i]);
+//           distanceX = abs(platformArr[i].x - platformArr[j].x);
+//           distanceY = abs(platformArr[i].y - platformArr[j].y);
+//
+//           if (distance < lngth + spacing) {
+//             if (distanceY < platformArr[i].width * 4) {
+//               console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
+//
+//               if (distanceX < (lngth + platformArr[j].length) / 2) {
+//                 console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
+//                 console.log("i: " + i + " OLD X POS: " + x);
+//                 console.log("i: " + i + " OLD Y POS: " + y);
+//                 console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
+//                 console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
+//
+//                 while (distanceY < platformArr[i].width * 4 &&
+//                   distanceX < (lngth + platformArr[j].length) / 2) {
+//                     lngth = random(80, 150);
+//                     spacing = random(30, 60);
+//                     x = (i * lngth) + spacing;
+//                     y = random(height * 0.25, height * 0.75);
+//                     platformArr[i] = new Platform(x, y, lngth);
+//
+//                     distanceX = abs(platformArr[i].x - platformArr[j].x);
+//                     distanceY = abs(platformArr[i].y - platformArr[j].y);
+//
+//                     console.log("i: " + i + " TOO CLOSE TO " + j + " ON Y AXIS");
+//                     console.log("i: " + i + " TOO CLOSE TO " + j + " ON X AXIS");
+//                     console.log("i: " + i + " OLD X POS: " + x);
+//                     console.log("i: " + i + " OLD Y POS: " + y);
+//                     console.log("j: " + j + " PLATFORM CHECKED X POS: " + platformArr[j].x);
+//                     console.log("j: " + j + " PLATFORM CHECKED Y POS: " + platformArr[j].y);
+//
+//                   }
+//
+//                 }
+//
+//                 console.log("");
+//
+//               }
+//
+//             }
+//
+//           }
+//
+//         }
+//
+//       }
+//
+//     }
+//
+//     return platformArr;
+//
+//   }
