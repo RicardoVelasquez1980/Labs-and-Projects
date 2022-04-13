4462735f92046041780197804848ef567aa9d3ce
Ricardo Velasco <ricardovelasquez681@gmail.com>
2022-04-13 07:50:08 -0700

    Revert "q"
    This reverts commit 40cc5a5dd06c8065049d03b82a5e3665419cd160.

--- Animation/sketch.js
+++ Animation/sketch.js
@@ -2,12 +2,10 @@
 //Animation
 
 let birdFrames = [];
-let frameIndex = 0;
 
 function preload(){
   for (let i = 0; i < 8; i++){
     birdFrames[i] = loadImage("b" + (i + 1) + ".png");
-    birdFrames[i].resize(width / 2, 0);
 
   }
 }
@@ -21,10 +19,11 @@ function setup() {
 function draw() {
   background(0);
 
-  imageMode(CENTER);
-  image(birdFrames[frameIndex], width / 2, height / 2);
+  for (let i = 0; i < birdFrames.length; i++){
+    image(birdFrames[i], (i * 100) + 100, height / 2);
 
-  frameIndex++;
-  if (frameIndex > birdFrames.length - 1) frameIndex = 0;
+  }
+
+  // image(birdFrames[0], width / 2, height / 2);
 
 }
